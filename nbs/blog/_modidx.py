700969bab472f5a5e4cac9df66954c86c63f1b3c
avivleemann <avivleemann@gmail.com>
2023-11-22 10:19:50 +0200

    readme render

--- nbs/blog/_modidx.py
+++ nbs/blog/_modidx.py
@@ -4,5 +4,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_baseurl': '/blog',
                 'doc_host': 'https://avivleemann.github.io',
                 'git_url': 'https://github.com/avivleemann/blog',
-                'lib_path': 'blog'},
+                'lib_path': 'nbs/blog'},
   'syms': {}}
